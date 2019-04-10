cf8d1feeb46164764a5d0a0ed863abab68490bac
Isaac Hayward <kodingroo@gmail.com>
2019-04-10 16:19:24 +0900

    seed setup

--- app/policies/game_archive_policy.rb
+++ app/policies/game_archive_policy.rb
@@ -4,4 +4,23 @@ class GameArchivePolicy < ApplicationPolicy
       scope.all
     end
   end
+
+  def show?
+    true
+  end
+
+  def update?
+    #only data_admin can update it
+    record.user == user
+  end
+
+  def create?
+    #Any logged in user can create a game_archive
+    #Any logged in user who is not disabled
+    !user.nil?
+  end
+
+  def destroy?
+    update?
+  end
 end
